4e1d24a13498ebaa155a7def660b32e2959a026e
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-12-30 21:05:04 +0800

    added create-user DTO and routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDTO) {
+    const { email, password } = body;
+    console.log(body);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDTO) {
+    const { email, password } = body;
+    console.log(body);
+  }
+}
