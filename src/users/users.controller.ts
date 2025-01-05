bb7b713b3ee3eed9ae3c2b3373fca25ee3be2ae8
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-05 18:50:04 +0800

    refactoring interceptor for less code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,7 @@ import {
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDTO } from './dtos/user.dto';
 
 @Controller('auth')
@@ -32,7 +32,7 @@ export class UsersController {
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
-  @UseInterceptors(new SerializeInterceptor(UserDTO))
+  @Serialize(UserDTO)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(+id);
