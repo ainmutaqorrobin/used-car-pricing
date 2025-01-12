5fd174ffefb3253b7582120d52547ba1929516c6
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-12 23:20:22 +0800

    testing for signin email does not exist

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService testing using fake service', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
   beforeEach(async () => {
     //Create fake copy of users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -33,5 +35,20 @@ describe('AuthService testing using fake service', () => {
     const [salt, hashedPassword] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hashedPassword).toBeDefined();
+  });
+
+  // it('throws error if user signup with existed email', async () => {
+  //   fakeUsersService.find = () =>
+  //     Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+  //   await expect(service.signup('test@gmail.com', 'test')).rejects.toThrow(
+  //     BadRequestException,
+  //   );
+  // });
+
+  it('throws if signin with email does not exist', async () => {
+    await expect(
+      service.signin('asdasdasdasd@gmail.com', 'test'),
+    ).rejects.toThrow(NotFoundException);
   });
 });
