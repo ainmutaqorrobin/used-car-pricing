ae303a1fc831047104f264ef5291f2ac56334ac0
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-15 20:22:58 +0800

    test findAllUsers, findUser method

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { User } from './users.entity';
 import { UsersController } from './users.controller';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -43,5 +44,22 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers return a list of users with entered email', async () => {
+    const users = await controller.findAllUsers('test@gmail.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('test@gmail.com');
+  });
+
+  it('findUser will return single user with entered id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+    expect(user.id).toEqual(1);
+  });
+
+  it('findUser throws not found exception if given id is not found', async () => {
+    fakeUsersService.findOne = (id) => Promise.reject(new NotFoundException());
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
