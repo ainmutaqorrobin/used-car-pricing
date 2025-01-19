9751c4ce86b0381e9eb5940d36b0f3fd52738a69
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-19 23:11:26 +0800

    added association between user and report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,13 +2,15 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDTO } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/users.entity';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDTO) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDTO, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
