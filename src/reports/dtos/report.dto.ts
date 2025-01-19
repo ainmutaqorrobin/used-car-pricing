9f88b0d247e949e98ad09f1a0e9c573cdd11550a
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-19 23:26:31 +0800

    formatting and transforming report response

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,24 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDTO {
+  @Expose()
+  id: number;
+  @Expose()
+  price: number;
+  @Expose()
+  year: number;
+  @Expose()
+  lng: number;
+  @Expose()
+  lat: number;
+  @Expose()
+  make: string;
+  @Expose()
+  model: String;
+  @Expose()
+  milleage: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,12 +4,15 @@ import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/users.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDTO } from './dtos/report.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDTO)
   createReport(@Body() body: CreateReportDTO, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
